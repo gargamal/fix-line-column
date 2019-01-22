afd2dcb51f00c3eea864bf6fd5cce29b502cf08c
PELLIER Daniel <d.pellier@akelio.fr>
2019-01-22 14:52:12 +0100

    Add hover line and column (no framework js necessary)

--- table.js
+++ table.js
@@ -14,10 +14,41 @@ const objFrozenHeadFirstLine = function () {
         that.childNodes[1].className = "noHighlightLine";
     }
 
+    const manageHighlightColumn = function (index, className) {
+        let myBody = document.getElementById('mybody');
+
+        for (let k = 0; k < myBody.childNodes.length; k++) {
+            let childLine = myBody.childNodes[k];
+            if (childLine.nodeName === 'TR') {
+                for (let i = 0, j = 0; i < childLine.childNodes.length; i++) {
+                    let childCell = childLine.childNodes[i];
+                    if (childCell.nodeName === 'TD') {
+                        if (index === j) {
+                            childCell.className = className;
+                            break;
+                        }
+                        j++;
+                    }
+                }
+            }
+        }
+
+    };
+
+    function highlightColumn(index) {
+        manageHighlightColumn(index, "highlightLine");
+    }
+
+    function noHighlightColumn(index) {
+        manageHighlightColumn(index, "");
+    }
+
     return {
         updateScroll,
         highlightLine,
         noHighlightLine,
+        noHighlightColumn,
+        highlightColumn,
     }
 };
 
