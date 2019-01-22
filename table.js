305af61c7863ada780d970cf8ef43c8d3e20e962
PELLIER Daniel <d.pellier@akelio.fr>
2019-01-22 13:01:38 +0100

    Fix - line change color on hover

--- table.js
+++ table.js
@@ -1,5 +1,24 @@
-function fixscroll() {
-    const thead = document.getElementById("myhead");
-    const tbodyScroll = document.getElementById("mybody").scrollLeft;
-    thead.scrollLeft = tbodyScroll;
-}
+const objFrozenHeadFirstLine = function () {
+    function updateScroll() {
+        const thead = document.getElementById("myhead");
+        thead.scrollLeft = document.getElementById("mybody").scrollLeft;
+    }
+
+    function highlightLine(that) {
+        that.className = "highlightLine";
+        that.childNodes[1].className = "highlightLine";
+    }
+
+    function noHighlightLine(that) {
+        that.className = "noHighlightLine";
+        that.childNodes[1].className = "noHighlightLine";
+    }
+
+    return {
+        updateScroll,
+        highlightLine,
+        noHighlightLine,
+    }
+};
+
+const FrozenHeadFirstLine = objFrozenHeadFirstLine();
